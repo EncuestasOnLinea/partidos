6e0abb50dcdf52299164748123ed01840b7181ab
EncuestasOnLinea <129552842+EncuestasOnLinea@users.noreply.github.com>
2023-03-31 14:54:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
-$(document).ready(function() {
-  // Definir un objeto de resultados vacío para almacenar los recuentos de votos y los porcentajes
-  var resultados = {
-    morena: { votos: 0, porcentaje: 0 },
-    verde: { votos: 0, porcentaje: 0 },
-    pri: { votos: 0, porcentaje: 0 },
-    pan: { votos: 0, porcentaje: 0 },
-    psi: { votos: 0, porcentaje: 0 }
-  };
-  
-  // Actualizar los recuentos de votos y porcentajes
-  function actualizarResultados() {
-    // Obtener el número total de votos
-    var totalVotos = Object.values(resultados).reduce(function(total, partido) {
-      return total + partido.votos;
-    }, 0);
+// Obtener los resultados almacenados en el local storage, o crear un objeto vacío si no hay resultados previos
+var resultados = JSON.parse(localStorage.getItem("resultados")) || {
+  morena: { votos: 0, porcentaje: 0 },
+  verde: { votos: 0, porcentaje: 0 },
+  pri: { votos: 0, porcentaje: 0 },
+  pan: { votos: 0, porcentaje: 0 },
+  psi: { votos: 0, porcentaje: 0 }
+};
 
-    // Actualizar los recuentos de votos y porcentajes para cada partido
-    $.each(resultados, function(partido, datos) {
-      var votos = datos.votos;
-      var porcentaje = (votos / totalVotos * 100).toFixed(2) + "%";
-      $("#" + partido + "-count").text(votos);
-      $("#" + partido + "-percentage").text(porcentaje);
-    });
-  }
-  
-  // Votar por un partido al hacer clic en su botón correspondiente
-  $(".vote-button").click(function() {
-    var partido = $(this).attr("id").replace("-button", "");
-    resultados[partido].votos++;
-    actualizarResultados();
+// Función para actualizar los recuentos de votos y porcentajes
+function actualizarResultados() {
+  // Obtener el número total de votos
+  var totalVotos = Object.values(resultados).reduce(function(total, partido) {
+    return total + partido.votos;
+  }, 0);
+
+  // Actualizar los recuentos de votos y porcentajes para cada partido
+  $.each(resultados, function(partido, datos) {
+    var votos = datos.votos;
+    var porcentaje = (votos / totalVotos * 100).toFixed(2) + "%";
+    $("#" + partido + "-count").text(votos);
+    $("#" + partido + "-percentage").text(porcentaje);
   });
-  
+
+  // Guardar los resultados actualizados en el local storage
+  localStorage.setItem("resultados", JSON.stringify(resultados));
+}
+
+// Función para votar por un partido
+function vote(partido) {
+  // Incrementar el recuento de votos para el partido correspondiente
+  resultados[partido].votos++;
+  // Actualizar los recuentos de votos y porcentajes
+  actualizarResultados();
+}
+
+// Esperar a que el documento esté listo
+$(document).ready(function() {
   // Actualizar los recuentos de votos y porcentajes al cargar la página
   actualizarResultados();
 });
