a971e15f8d6d3cb914495a65eeee72bdd22ffb91
EncuestasOnLinea <129552842+EncuestasOnLinea@users.noreply.github.com>
2023-04-02 11:14:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,141 @@
+// Acceder a los elementos del DOM
 const startButton = document.getElementById('startButton');
 const stopButton = document.getElementById('stopButton');
 const volumeSlider = document.getElementById('volumeSlider');
 
+// Establecer las credenciales de acceso al servidor
 const serverAddress = 'fluoz.zeno.fm';
-const port = '80';
+const serverPort = 80;
 const mountPoint = 'n932reythxhvv/source';
 const username = 'source';
 const password = 'eenWG5kW';
-const encoding = 'mp3';
+const encoding = 'MP3';
 
-let mediaRecorder;
-let recordedChunks = [];
-let audioStream;
+// Crear un objeto AudioContext
+const audioContext = new AudioContext();
 
-startButton.addEventListener('click', async () => {
-  try {
-    audioStream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    mediaRecorder = new MediaRecorder(audioStream, { mimeType: `audio/${encoding}` });
+// Crear un nodo de ganancia y establecer el valor inicial
+const gainNode = audioContext.createGain();
+gainNode.gain.value = volumeSlider.value;
 
-    mediaRecorder.addEventListener('dataavailable', event => {
-      if (event.data.size > 0) {
-        recordedChunks.push(event.data);
-      }
+// Crear un nodo de destino para el flujo de audio
+const destination = audioContext.createMediaStreamDestination();
+
+// Crear un objeto Recorder y asignarle la fuente de audio
+const recorder = new Recorder(gainNode, {
+  encoderPath: 'https://cdn.jsdelivr.net/npm/@vocoder-js/mp3@1.1.1/dist/vocoder.min.js'
+});
+recorder.setDestination(destination);
+
+// Habilitar/deshabilitar botones
+toggleButtons(false, true);
+
+// Agregar un controlador de eventos al botón de encendido
+startButton.addEventListener('click', () => {
+  // Habilitar/deshabilitar botones
+  toggleButtons(true, false);
+
+  // Acceder al micrófono del dispositivo
+  navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(stream => {
+      // Crear un nodo de origen para el flujo de audio
+      const source = audioContext.createMediaStreamSource(stream);
+      source.connect(gainNode);
+      gainNode.connect(destination);
+
+      // Establecer los parámetros de configuración del objeto Recorder
+      recorder.config.sampleRate = audioContext.sampleRate;
+      recorder.config.channels = 1;
+      recorder.config.format = encoding;
+      
+      // Conectar el nodo de ganancia al objeto Recorder
+      recorder.record();
+
+      // Transmitir el audio al servidor
+      const audioElement = document.createElement('audio');
+      audioElement.srcObject = destination.stream;
+      audioElement.controls = true;
+      audioElement.autoplay = true;
+      document.body.appendChild(audioElement);
+      audioElement.play();
+      
+      // Crear un objeto ICEcast y establecer las credenciales de acceso
+      const icecast = new Icecast({
+        server: serverAddress,
+        port: serverPort,
+        mount: mountPoint,
+        username: username,
+        password: password,
+        format: encoding,
+        name: 'My Stream',
+        url: 'https://www.example.com/stream',
+        genre: 'Misc',
+        description: 'My awesome stream'
+      });
+
+      // Conectar el objeto Recorder al servidor
+      icecast.on('ready', () => {
+        recorder.stream().pipe(icecast);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+      // Habilitar/deshabilitar botones
+      toggleButtons(false, true);
     });
-
-    mediaRecorder.addEventListener('stop', () => {
-      const audioBlob = new Blob(recordedChunks, { type: `audio/${encoding}` });
-      const audioUrl = URL.createObjectURL(audioBlob);
-
-      const xhr = new XMLHttpRequest();
-      xhr.open('POST', `http://${serverAddress}:${port}/${mountPoint}`);
-      xhr.setRequestHeader('Authorization', `Basic ${btoa(`${username}:${password}`)}`);
-      xhr.send(audioBlob);
-
-      recordedChunks = [];
-      URL.revokeObjectURL(audioUrl);
-    });
-
-    mediaRecorder.start();
-    startButton.disabled = true;
-    stopButton.disabled = false;
-    volumeSlider.disabled = true;
-  } catch (err) {
-    console.error(err);
-  }
 });
 
+// Agregar un controlador de eventos al botón de apagado
 stopButton.addEventListener('click', () => {
-  mediaRecorder.stop();
-  startButton.disabled = false;
-  stopButton.disabled = true;
-  volumeSlider.disabled = false;
-  audioStream.getTracks().forEach(track => track.stop());
+  // Habilitar/deshabilitar botones
+  toggleButtons(false, true);
+
+  // Detener la grabación
+  recorder.stop();
+  
+  // Detener la transmisión
+  icecast.on('end', () => {
+    icecast.disconnect();
+  });
 });
+
+// Agregar un controlador de eventos al control deslizante de volumen
+volumeSlider.addEventListener('input', function(e) {
+// Establecer el volumen del objeto de audio en función del valor del control deslizante
+audioElement.volume = e.target.value;
+});
+
+// Agregar un controlador de eventos al botón "Encender"
+startButton.addEventListener('click', function() {
+// Habilitar/deshabilitar botones
+toggleButtons(false, true);
+
+// Obtener la secuencia de audio del servidor
+var streamUrl = 'http://' + serverAddress + ':' + serverPort + '/' + mountPoint;
+
+// Configurar el objeto de audio con la secuencia de audio
+audioElement.src = streamUrl;
+audioElement.type = 'audio/' + encoding;
+
+// Autenticar con el servidor
+var authString = btoa(username + ':' + password);
+audioElement.setRequestHeader('Authorization', 'Basic ' + authString);
+
+// Comenzar a reproducir la secuencia de audio
+audioElement.play();
+});
+
+// Agregar un controlador de eventos al botón "Apagar"
+stopButton.addEventListener('click', function() {
+// Habilitar/deshabilitar botones
+toggleButtons(true, false);
+
+// Detener la reproducción de la secuencia de audio
+audioElement.pause();
+});
+
+// Función para habilitar/deshabilitar los botones de "Encender" y "Apagar"
+function toggleButtons(start, stop) {
+startButton.disabled = !start;
+stopButton.disabled = !stop;
+}
