fc008b6431b295e1c6455a3144438eece470411d
EncuestasOnLinea <129552842+EncuestasOnLinea@users.noreply.github.com>
2023-04-02 10:53:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,55 @@
+// Establecer variables de configuración
+const server = 'fluoz.zeno.fm';
+const port = 80;
+const mountPoint = 'n932reythxhvv/source';
+const username = 'source';
+const password = 'eenWG5kW';
+const encoding = 'MP3';
+
+// Obtener elementos del DOM
+const volumeSlider = document.getElementById('volumeSlider');
 const startButton = document.getElementById('startButton');
 const stopButton = document.getElementById('stopButton');
-const volumeSlider = document.getElementById('volumeSlider');
-const audioContext = new AudioContext();
-const icecastUrl = 'http://fluoz.zeno.fm:80/n932reythxhvv/source';
 
-let mediaRecorder;
-let audioChunks = [];
-let sourceNode;
-let gainNode;
-let audioStream;
+// Establecer el contexto de audio
+const audioCtx = new AudioContext();
+const source = audioCtx.createBufferSource();
+const gainNode = audioCtx.createGain();
+const analyser = audioCtx.createAnalyser();
 
-const constraints = { audio: true };
+// Conectar el nodo de ganancia y el analizador al contexto de audio
+source.connect(gainNode);
+gainNode.connect(analyser);
+analyser.connect(audioCtx.destination);
 
+// Establecer la configuración de la solicitud de Icecast
+const icecastURL = `http://${server}:${port}/${mountPoint}`;
+const icecastRequest = new Request(icecastURL, {
+  method: 'PUT',
+  headers: {
+    'Authorization': `Basic ${btoa(`${username}:${password}`)}`,
+    'Content-Type': `audio/${encoding}`,
+  },
+});
+
+// Iniciar la transmisión de audio
 startButton.addEventListener('click', () => {
-  navigator.mediaDevices.getUserMedia(constraints)
-    .then(stream => {
-      audioStream = stream;
+  navigator.mediaDevices.getUserMedia({audio: true})
+    .then((stream) => {
+      const audioSource = audioCtx.createMediaStreamSource(stream);
+      audioSource.connect(gainNode);
       startButton.disabled = true;
       stopButton.disabled = false;
-      mediaRecorder = new MediaRecorder(stream);
-      mediaRecorder.addEventListener('dataavailable', event => {
-        audioChunks.push(event.data);
-      });
-      mediaRecorder.addEventListener('stop', () => {
-        const audioBlob = new Blob(audioChunks);
-        const audioUrl = URL.createObjectURL(audioBlob);
-        const audio = new Audio(audioUrl);
-        sourceNode = audioContext.createMediaElementSource(audio);
-        gainNode = audioContext.createGain();
-        sourceNode.connect(gainNode);
-        gainNode.connect(audioContext.destination);
-        audio.play();
-        audioChunks = [];
-      });
-      mediaRecorder.start();
+      return audioCtx.resume();
     })
-    .catch(console.error);
+    .then(() => fetch(icecastRequest))
+    .catch((err) => console.error('Error:', err));
 });
 
+// Detener la transmisión de audio
 stopButton.addEventListener('click', () => {
+  gainNode.gain.value = 0;
   startButton.disabled = false;
   stopButton.disabled = true;
-  mediaRecorder.stop();
-  audioStream.getTracks().forEach(track => track.stop());
-});
-
-volumeSlider.addEventListener('input', () => {
-  if (gainNode) {
-    gainNode.gain.value = volumeSlider.value;
-  }
-});
-
-function sendAudioToIcecast(audioBlob) {
-  const formData = new FormData();
-  formData.append('file', audioBlob, 'audio.mp3');
-
-  fetch(icecastUrl, {
-    method: 'PUT',
-    body: formData,
-    headers: {
-      'Authorization': 'Basic ' + btoa('source:eenWG5kW'),
-    },
-  })
-    .then(response => {
-      if (response.ok) {
-        console.log('Audio sent to Icecast successfully!');
-      } else {
-        console.error('Error sending audio to Icecast.');
-      }
-    })
-    .catch(error => {
-      console.error('Error sending audio to Icecast:', error);
-    });
-}
-
-mediaRecorder.addEventListener('dataavailable', event => {
-  sendAudioToIcecast(event.data);
+  fetch(icecastURL, {method: 'DELETE'});
 });
